1f784e5de32ff8f1e385d437f18331e0c4fa6d1f
luckymurphydevelopment <luckymurphydevelopment@gmail.com>
2020-11-21 21:11:10 -0700

    stuff

--- js/board.js
+++ js/board.js
@@ -3,14 +3,23 @@ class Board {
         this.data = [];
         this.generate();
     }
+    
+    shuffle(array) {
+        for(let i = array.length - 1; i > 0; i--){
+            const j = Math.floor(Math.random() * i);
+            const temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+          }
+    }
 
     generate() {
-        for (let i = 0; i < this.data.length; i++) {
+        for (let i = 0; i < 9; i++) {
             this.data[i] = [];
-            for (let j = 0; j < this.data[i].length; j++) {
+            for (let j = 0; j < 9; j++) {
                 this.data[i].push(j);
             }
-            shuffle(data[i]);
+            this.shuffle(this.data[i]);
         }
     }
 
@@ -25,46 +34,49 @@ class Board {
     // }
 
     draw(ctx) {
-        let cellSize = Math.floor(Math.min(ctx.canvas.width, ctx.canvas.height) / 9);
+        let cellSize = Math.floor(Math.min(ctx.canvas.width - 18, ctx.canvas.height - 18) / 9);
         let origX = Math.floor(ctx.canvas.width / 2 - cellSize * 4.5);
         let origY = Math.floor(ctx.canvas.height / 2 - cellSize * 4.5);
+        for (let i = 0; i < 10; i++) {
+            line(ctx, origX + cellSize * i, origY, origX + cellSize * i, origY + cellSize * 9, i);
+        }
+        for (let j = 0; j < 10; j++) {
+            line(ctx, origX, origY + cellSize * j, origX + cellSize * 9, origY + cellSize * j, j);
+        }
+        set_font(ctx, cellSize);
         for (let i = 0; i < this.data.length; i++) {
-            if (i != this.data.length - 1) { // draw vertical lines
-                line(ctx, origX + cellSize * i, origY, origX + celLSize * i, origY + cellSize * 9);
-            }
             for (let j = 0; j < this.data[i].length; j++) {
-                if (i == 0) { // draw horizontal lines
-                    line(ctx, origX, origY + cellSize * i, origX + cellSize * 9, origY + celLSize * i);
-                }
-                // text(this.data[i][j], origX + cellSize * i + cellSize * 0.5, origY + cellSize * j + cellSize * 0.5);
+                draw_text(ctx, this.data[j][i], origX + cellSize * i + cellSize * 0.5, origY + cellSize * j + cellSize * 0.5);
             }
         }
     }
 
-    shuffle(array){
-        for(let i = array.length - 1; i > 0; i--){
-            const j = Math.floor(Math.random() * i);
-            const temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-          }
-    }
 }
 
-function line(ctx, x1, y1, x2, y2) {
+function line(ctx, x1, y1, x2, y2, num) {
+    if (num % 3 == 0) {
+        ctx.lineWidth = 3;
+    } else {
+        ctx.lineWidth = 1;
+    }
     ctx.beginPath();
     ctx.strokeStyle = "black";
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
+    ctx.moveTo(Math.round(x1), Math.round(y1));
+    ctx.lineTo(Math.round(x2), Math.round(y2));
     ctx.stroke();
     ctx.closePath();
 }
 
-function text(text, x, y) {
+function draw_text(ctx, text, x, y) {
     ctx.beginPath();
-    ctx.fillStyle = "black";
-    ctx.textAlign = "center";
     ctx.fillText(text, x, y);
     ctx.closePath();
+}
 
+function set_font(ctx, cellSize) {
+    let size = Math.round(cellSize * 0.75);
+    ctx.font = size + "px Arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillStyle = "black";
 }
