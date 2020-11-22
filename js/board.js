f95baedcaf4c9c0a96bb2751f8302cab15782592
canyonbryson <canyonbryson@yahoo.com>
2020-11-21 20:49:39 -0700

    rows

--- js/board.js
+++ js/board.js
@@ -1,11 +1,28 @@
 class Board {
     constructor() {
+        this.data = [];
         this.generate();
     }
 
     generate() {
-
+        for (let i = 0; i < this.data.length; i++) {
+            this.data[i] = [];
+            for (let j = 0; j < this.data[i].length; j++) {
+                this.data[i].push(j);
+            }
+            shuffle(data[i]);
+        }
     }
+
+    // validate(data){
+    //     for (let i = 0; i < this.data.length; i++) {
+    //         this.data[i] = [];
+    //         for (let j = 0; j < this.data[i].length; j++) {
+    //             this.data[i].push(j);
+    //         }
+    //         shuffle(data[i]);
+    //     }
+    // }
 
     draw(ctx) {
         for (let i = 0; i < this.data.length; i++) {
@@ -13,5 +30,14 @@ class Board {
 
             }
         }
+    }
+
+    shuffle(array){
+        for(let i = array.length - 1; i > 0; i--){
+            const j = Math.floor(Math.random() * i);
+            const temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+          }
     }
 }
