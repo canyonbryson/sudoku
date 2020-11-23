ccac0bfd3772f3407f31f6981bf4b86bcffd4db5
luckymurphydevelopment <luckymurphydevelopment@gmail.com>
2020-11-23 16:28:42 -0700

    a

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 window.onload = function() {
+    var slider = document.getElementById("m1");
+    let difficulty = get_cookie("difficulty", 1); // does not work on local files, only on www
 
     var cvs = document.querySelector("#mainCanvas");
     cvs.width = window.innerWidth;
@@ -6,6 +8,5 @@ window.onload = function() {
     var ctx = cvs.getContext('2d');
     var board = new Board();
     board.draw(ctx);
-    console.log(board.data);
 
 };
