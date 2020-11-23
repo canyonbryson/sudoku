af0d18b3520704a12ac68b7e2933ec6df31f5127
luckymurphydevelopment <luckymurphydevelopment@gmail.com>
2020-11-23 14:45:46 -0700

    button and clear canvas

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 window.onload = function() {
-
     var slider = document.getElementById("m1");
     let difficulty = parseInt(slider.value);
-    slider.oninput = function() {
-        difficulty = parseInt(slider.value);
-        // ctx.clearRect(0, 0, width, height);
-        var board = new Board(difficulty);
-        board.draw(ctx);
-        console.log(board.data);
-    }
 
     var cvs = document.querySelector("#mainCanvas");
     cvs.width = window.innerWidth;
@@ -17,7 +9,16 @@ window.onload = function() {
     var board = new Board(difficulty);
     board.draw(ctx);
     console.log(board.data);
-
     
-
+    slider.oninput = function() {
+        difficulty = parseInt(slider.value);
+    };
+    
+    var button = document.getElementById("btnGenerate");
+    button.addEventListener("click", function() {
+        ctx.clearRect(0, 0, cvs.width, cvs.height);
+        board = new Board(difficulty);
+        board.draw(ctx);
+        // console.log(board.data);
+    });
 };
